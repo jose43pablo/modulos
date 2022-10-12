2ee1dc7a098ff682e5cedfd0ab3668cd18f6ae59
Pablo <josepablo43@outlook.com>
2022-10-11 23:28:26 -0700

    Commit1: Se creo zen372.py, modulo calculator con minus.py y plus.py

---
+++ main.py
@@ -0,0 +1,5 @@
+# Script = Modulo (archivo.py)
+
+#Paquete:
+import calculator.plus
+import calculator.minus
---
+++ zen372.py
@@ -0,0 +1,15 @@
+import this
+# Comentario de una sola linea
+
+"""Comentatio de varias lineas
+linea 1
+linea 2
+linea ....."""
+
+print("Latosa1: No se")
+print("Latosa2: Mas vale pedir perdon que pedir permiso")
+print("Marco1: Camaron que se duerme bla bla bla")
+print("Marco2: Fuga!!!")
+print("Latosa1: Fuga x 2")
+
+barba = "Algo que sorpresa!!"
